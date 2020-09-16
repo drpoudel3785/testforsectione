b260aa950dec52c1584a300094134bfd61befdc1
drpoudel3785 <dharmarajpoudel@hotmail.com>
2020-09-16 12:36:05 +0545

    DOM and Basic form validations

--- scripts/my.js
+++ scripts/my.js
@@ -152,4 +152,49 @@ function sumT() {
       document.getElementById("ss").innerHTML=ss;
   }
 
+  function bmw(){
+      document.getElementById("car1").src="images/bmw1.png";
+  }
+  function car(){
+    document.getElementById("car1").src="images/car1.png";
+}
+
+function changeBg() {
+    document.body.style.backgroundColor="#ff0000";
+
+}
+
+document.body.onload = addElement;
+
+function addElement () { 
+  // create a new div element 
+  const newDiv = document.createElement("div"); 
+  
+  // and give it some content 
+  const newContent = document.createTextNode("Hi there and greetings!"); 
+  
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);  
+
+  // add the newly created element and its content into the DOM 
+  const currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+  return false;
+}
+
+
+function validateLogin() {
+    //     alert("Form Submitted");
+    //Getting the data from form DOM
+    var uname = document.login.user.value;
+    var upass = document.login.pass.value;
+
+    //checking the form elements are filled or not
+    if(uname == "" && upass == "") {
+        document.getElementById("loginError").innerHTML="Username and Password is required";
+        document.login.user.focus();
+        return false;
+    }
+    return true;
+}
 
